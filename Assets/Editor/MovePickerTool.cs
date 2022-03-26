bfd5ca8981051d730710c8bf667b06a5d8ace379
JT5519 <joel.sk10@gmail.com>
2022-03-26 01:52:57 -0400

    Monster and Move States and Utlities added

---
+++ Assets/Editor/MovePickerTool.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+[CustomEditor(typeof(MonsterBehavior))]
+public class MovePickerTool : Editor
+{
+    MoveManager myMoveManagerScript;
+    int move1Index = 0;
+    int move2Index = 0;
+    int move3Index = 0;
+    int move4Index = 0;
+    
+    public override void OnInspectorGUI()
+    {
+        // Draw the default inspector
+        DrawDefaultInspector();
+
+        //get initial values
+        MonsterBehavior monsterBehaviorScript = (MonsterBehavior)target;
+        myMoveManagerScript = FindObjectOfType<MoveManager>();
+        Move[] allMoves = myMoveManagerScript.allMovesInGame.ToArray();
+        string[] moveNames = myMoveManagerScript.moveNames.ToArray();
+
+        //set moveIndices
+        move1Index = Math.Max(Array.IndexOf(allMoves,monsterBehaviorScript.moveSet[0]),0);
+        move2Index = Math.Max(Array.IndexOf(allMoves, monsterBehaviorScript.moveSet[1]), 0);
+        move3Index = Math.Max(Array.IndexOf(allMoves, monsterBehaviorScript.moveSet[2]), 0);
+        move4Index = Math.Max(Array.IndexOf(allMoves, monsterBehaviorScript.moveSet[3]), 0);
+
+        //allow choices
+        move1Index = EditorGUILayout.Popup("Move 1",move1Index, moveNames);
+        move2Index = EditorGUILayout.Popup("Move 2", move2Index, moveNames);
+        move3Index = EditorGUILayout.Popup("Move 3", move3Index, moveNames);
+        move4Index = EditorGUILayout.Popup("Move 4", move4Index, moveNames);
+
+        //Update choice
+        monsterBehaviorScript.moveSet[0] = allMoves[move1Index];
+        monsterBehaviorScript.moveSet[1] = allMoves[move2Index];
+        monsterBehaviorScript.moveSet[2] = allMoves[move3Index];
+        monsterBehaviorScript.moveSet[3] = allMoves[move4Index];                
+    }
+}
+
---
+++ Assets/Joel Scripts/MoveManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveManager : MonoBehaviour
+{
+    [HideInInspector] public List<string> moveNames;
+    public List<Move> allMovesInGame;
+    private void OnValidate()
+    {
+        allMovesInGame.Clear();
+        moveNames.Clear();
+        AddNewMove("Flamethrower", Type.Volcano, 6, new Stats(0, 0, 0, 0), 10);
+        AddNewMove("Hydro Pump", Type.Swamp, 6, new Stats(0, 0, 0, 0), 10);
+    }
+    private void AddNewMove(string moveName, Type moveType,int baseDamage, Stats myStats, int critProb)
+    {
+        allMovesInGame.Add(new Move(moveName, moveType, baseDamage, myStats, critProb));
+        moveNames.Add(moveName);
+    }
+}
